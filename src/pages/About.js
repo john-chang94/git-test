d1080f064e06c3f85b5689283d05ec83181ea995
john-chang94 <j.chang94@yahoo.com>
2022-06-30 16:33:25 -0700

    fetch posts and render on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default About
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await axios(`https://jsonplaceholder.typicode.com/posts`);
+      const data = res.data.slice(0, 10);
+      setPosts(data);
+    };
+
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+        {posts.map(post => (
+            <div>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ))}
+    </div>
+  )
+};
+
+export default About;
