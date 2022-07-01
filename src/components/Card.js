c05c5f54c83ac6268b9400e100ee5177448771b5
john-chang94 <39566708+john-chang94@users.noreply.github.com>
2022-06-30 21:17:38 -0700

    add card and update navbar

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+export const Card = ({ title, body }) => (
+    <div>
+        <div>
+            <h2>{title}</h2>
+        </div>
+        <div>
+            <p>{body}</p>
+        </div>
+    </div>
+)
