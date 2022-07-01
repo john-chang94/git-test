2afd6387ac1befd026cefc04f33bc7906c54074a
john-chang94 <39566708+john-chang94@users.noreply.github.com>
2022-06-30 21:53:16 -0700

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
+        tempore, adipisci dolorum illum neque sequi minima cumque quae earum
+        laboriosam voluptatem id et, iste inventore possimus voluptatibus
+        ratione necessitatibus nesciunt.
+      </p>
+    </div>
+  );
+};
